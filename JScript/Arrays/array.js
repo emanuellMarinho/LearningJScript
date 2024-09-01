71d2d863bc72e5b55185bc65107b5a2cb863181f
emanuellMarinho <joaoemanuelribeiromarinho@gmail.com>
2024-09-01 20:48:54 -0300

    Revisando Arrays

---
+++ JScript/Arrays/array.js
@@ -0,0 +1,15 @@
+// Valor por referência
+// //                 0         1         2           3         4        5       6
+// const nomes = ['Emanuel', 'Jonas', 'Jeronimo', 'Danilton', 'Erik', 'Ávila', 'Dani']
+// //                -7        -6        -5          -4        -3       -2      -1
+// const novo = nomes.slice(0, -5)
+// console.log(novo)
+
+// Converte uma String em um Array
+const nome = 'Emanuel Marinho'
+const nomes = nome.split(' ')
+
+// Converte um Array em uma String
+// const nomes = ['Emanuel', 'Marinho']
+// const nome = nomes.join(' ')
+console.log(nomes)
